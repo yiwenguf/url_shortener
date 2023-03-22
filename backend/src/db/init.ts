a572cfca2eb0dca32dde450226f0f44725694a26
Yi Weng <yiweng.chow@ufinity.com>
2023-03-22 17:22:03 +0800

    Bug fix for db

--- backend/src/db/init.ts
+++ backend/src/db/init.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 import Links, { initLinks } from './models/links';
 
-const initSequelizeConnection = (db_host: string, db_name: string, db_user: string, db_pass: string): Sequelize => new Sequelize(db_name, db_user, db_user, {
+const initSequelizeConnection = (db_host: string, db_name: string, db_user: string, db_pass: string): Sequelize => new Sequelize(db_name, db_user, db_pass, {
   host: db_host,
   dialect: 'mysql',
 });
